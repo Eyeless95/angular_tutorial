892b4427fd45364fbd1f776e6ba96c2e426149f2
ZagrebinB <zagrebin.bogdan@gmail.com>
2016-07-07 12:27:52 +0300

    use mixins and generics

--- core/urls.py
+++ core/urls.py
@@ -1,17 +1,13 @@
 from django.conf.urls import include, url
 from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
-from rest_framework import routers
-
-
-router = routers.DefaultRouter()
-router.register(r'account', views.AccountViewSet)
 
 
 app_name = 'core'
 urlpatterns = [
-    url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^accounts/$', views.accounts_list),
-    url(r'^accounts/(?P<pk>[0-9]+)$', views.account_detail),
+    url(r'^accounts/$', views.AccountList.as_view()),
+    url(r'^accounts/(?P<pk>[0-9]+)/$', views.AccountDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- core/views.py
+++ core/views.py
@@ -1,12 +1,7 @@
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from .models import Account
 from rest_framework import viewsets
 from .serializers import AccountSerializer
+from rest_framework import generics
 
 
 class AccountViewSet(viewsets.ModelViewSet):
@@ -17,44 +12,13 @@ class AccountViewSet(viewsets.ModelViewSet):
     serializer_class = AccountSerializer
 
 
-@api_view(['GET', 'POST'])
-def accounts_list(request):
-    if request.method == 'GET':
-        accounts = Account.objects.all()
-        serializer = AccountSerializer(accounts, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = AccountSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class AccountList(generics.ListCreateAPIView):
+
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def account_detail(request, pk):
-    """
-    Retrieve, update or delete a code snippet.
-    """
-    try:
-        account = Account.objects.get(pk=pk)
-    except Account.DoesNotExist:
-        return HttpResponse(status=404)
+class AccountDetail(generics.RetrieveUpdateDestroyAPIView):
 
-    if request.method == 'GET':
-        serializer = AccountSerializer(account)
-        return Response(serializer.data)
-
-    elif request.method == 'PUT':
-        serializer = AccountSerializer(account, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        account.delete()
-        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
-
-
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
